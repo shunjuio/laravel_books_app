49768ad53326c488372c475da18075dfe1b7a9a5
mugiyuki <yukimugikura@gmail.com>
2023-05-18 12:39:20 +0900

    予約時に確認メール送信機能の追加

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\ReservationReminder;
 use App\Models\Reservation;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Book;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Storage;
 use PhpParser\Builder;
 
@@ -22,8 +24,8 @@ class ReservationController extends Controller
 
     public function show(Request $request, $reservationId)
     {
-        $user = Auth::user();
-        $reservation = $user->reservations()->where('id', $reservationId)->with('book')->first();
+        $user                            = Auth::user();
+        $reservation                     = $user->reservations()->where('id', $reservationId)->with('book')->first();
         $reservation->display_image_path = Storage::url($reservation->book->image_path);
         $reservation->display_start_at   = Carbon::parse($reservation->start_at)->format('Y-m-d');
         $reservation->display_end_at     = Carbon::parse($reservation->end_at)->format('Y-m-d');
@@ -40,6 +42,11 @@ class ReservationController extends Controller
             'start_at' => $request->get('start_at'),
             'end_at'   => $request->get('end_at')
         ]);
+
+        $reservation = $user->reservations()->where('book_id', $bookId)->with('book')->first();
+        $reservation->display_start_at   = Carbon::parse($reservation->start_at)->format('Y-m-d');
+dd($reservation);
+        Mail::to($user)->send(new ReservationReminder($reservation));
 
         return redirect()->route('reservations.index');
     }
--- app/Mail/ReservationReminder.php
+++ app/Mail/ReservationReminder.php
@@ -2,12 +2,15 @@
 
 namespace App\Mail;
 
+use App\Models\Reservation;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Address;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
+
 
 class ReservationReminder extends Mailable
 {
@@ -16,7 +19,9 @@ class ReservationReminder extends Mailable
     /**
      * Create a new message instance.
      */
-    public function __construct()
+    public function __construct(
+        protected Reservation $reservation,
+    )
     {
         //
     }
@@ -24,20 +29,26 @@ class ReservationReminder extends Mailable
     /**
      * Get the message envelope.
      */
-    public function envelope(): Envelope
+    public function envelope() : Envelope
     {
         return new Envelope(
+            from: new Address('example@example.com', 'Book Manager'),
             subject: 'Reservation Reminder',
         );
     }
 
+
     /**
      * Get the message content definition.
      */
-    public function content(): Content
+    public function content() : Content
     {
         return new Content(
-            view: 'view.name',
+            view: 'emails.reservation.reminder',
+            with: [
+                'bookTitle'            => $this->reservation->book->title,
+                'reservationStartDate' => $this->reservation->display_start_at,
+            ],
         );
     }
 
@@ -46,7 +57,7 @@ class ReservationReminder extends Mailable
      *
      * @return array<int, \Illuminate\Mail\Mailables\Attachment>
      */
-    public function attachments(): array
+    public function attachments() : array
     {
         return [];
     }
