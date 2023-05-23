bbb877786cf5959e071415fa99da706f584ac29e
Ai Onaga <a.onaga@shunju.io>
2023-05-23 13:06:24 +0900

    レビュー箇所修正

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-         $schedule->command('remind:lending')->daily()->dailyAt('12:00');
+         $schedule->command('remind:lending')->dailyAt('12:00');
     }
 
     /**
